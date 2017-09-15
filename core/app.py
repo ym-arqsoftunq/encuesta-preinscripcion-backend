5f3a07b217fc320ef61bd736f610882c83a040f8
Nacho Yegro <juan.yegro@unq.edu.ar>
2017-09-15 11:07:50 -0300

    Commit inicial y travis

---
+++ core/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+alumnos = [
+    {
+        'id': 1,
+        'nombre': u'Juan Ignacio Yegro',
+        'materias': [1, 3]
+    },
+    {
+        'id': 2,
+        'nombre': u'Nestor Munoz',
+        'materias': [2, 3]
+    }
+]
+
+materias = [
+    {
+        'id': 1,
+        'nombre': u'Arquitectura de Software 1',
+        'alumnos': [1]
+    },
+    {
+        'id': 2,
+        'nombre': u'Aspectos legales y sociales',
+        'alumnos': [2]
+    },
+    {
+        'id': 3,
+        'nombre': u'Probabilidad y estadistica',
+        'alumnos': [1, 2]
+    },
+]
+
+@app.route('/alumnos', methods=['GET'])
+def get_alumnos():
+    return jsonify({'alumnos': alumnos})
+
+@app.route('/materias', methods=['GET'])
+def get_materias():
+    return jsonify({'materias': materias})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
