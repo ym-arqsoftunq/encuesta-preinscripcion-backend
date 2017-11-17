9c362300641248afd4ab2923f973a827f3e45709
Nacho Yegro <nachoyegro@gmail.com>
2017-11-17 14:26:40 -0300

    Pongo alumno en encuesta nueva

--- core/repository.py
+++ core/repository.py
@@ -3,10 +3,12 @@ import json
 
 class Repository(object):
 
-    def get_oferta(self, oferta_id):
+    def get_oferta(self, oferta_id, username):
         SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
         json_url = os.path.join(SITE_ROOT, "encuestas/oferta_%s.json" % (oferta_id))
-        return json.load(open(json_url))
+        encuesta = json.load(open(json_url))
+        encuesta['alumno'] = {'username': username}
+        return encuesta
 
     def get_encuesta_alumno(self, oferta_id, alumno_id):
         try:
@@ -14,7 +16,7 @@ class Repository(object):
             json_url = os.path.join(SITE_ROOT, "encuestas/oferta_%s_alumno_%s.json" % (oferta_id, alumno_id))
             data = json.load(open(json_url))
         except:
-            data = self.get_oferta(oferta_id)
+            data = self.get_oferta(oferta_id, alumno_id)
         return data
 
     def guardar_encuesta_alumno(self, encuesta):
