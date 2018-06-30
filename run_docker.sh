277213fffe7f538125774ddc63b0d1502bd39360
Juan Ignacio Yegro <juan.yegro@unq.edu.ar>
2018-06-29 23:47:07 -0300

    Agrego fixtures al script

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo killing old docker processes
+echo Matando procesos viejos
 docker-compose rm -fs
 
-echo building docker containers
+echo Creando containers nuevos
 docker-compose up --build -d
+
+echo Corriendo fixtures
+docker exec app-backend bash -c 'cd core ; python manage.py db upgrade; python fixtures.py'
